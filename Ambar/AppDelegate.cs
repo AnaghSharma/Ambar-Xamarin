7687ee61efaaff5c7097da52ad2930d54c030c90
AnaghSharma <anaghsharma12@gmail.com>
2017-06-11 22:42:03 +0530

    Added icon for Status Bar Item

--- Ambar/AppDelegate.cs
+++ Ambar/AppDelegate.cs
@@ -6,18 +6,20 @@ namespace Ambar
     [Register("AppDelegate")]
     public class AppDelegate : NSApplicationDelegate
     {
+        NSStatusBar statusBar = new NSStatusBar();
+       
         public AppDelegate()
         {
         }
 
         public override void DidFinishLaunching(NSNotification notification)
         {
-            // Insert code here to initialize your application
-            NSStatusBar statusBar = new NSStatusBar();
+			// Insert code here to initialize your application
+			NSStatusItem statusItem = statusBar.CreateStatusItem(NSStatusItemLength.Variable);
 
-            var statusItem = statusBar.CreateStatusItem(NSStatusItemLength.Variable);
-            statusItem.Title = "Ambar";
-            statusItem.HighlightMode = true;
+            var button = statusItem.Button;
+            button.Image = new NSImage("StatusBarIcon@2x.png");
+
         }
 
         public override void WillTerminate(NSNotification notification)
