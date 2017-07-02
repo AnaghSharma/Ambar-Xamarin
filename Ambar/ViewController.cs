10b94f34d51e55a73c5d48611094b0d95d731336
AnaghSharma <anaghsharma12@gmail.com>
2017-07-02 20:16:57 +0530

    Added method for Launch at Login setting

--- Ambar/ViewController.cs
+++ Ambar/ViewController.cs
@@ -41,6 +41,11 @@ namespace Ambar
             }
         }
 
-        partial void SettingsButtonClick(NSObject sender)         {             var current = NSApplication.SharedApplication.CurrentEvent;              NSMenu settingsMenu = new NSMenu();              NSMenuItem launch = new NSMenuItem("Launch at Login", new ObjCRuntime.Selector("launch:"), "");             NSMenuItem quit = new NSMenuItem("Quit", new ObjCRuntime.Selector("quit:"), "q");              settingsMenu.AddItem(launch);             settingsMenu.AddItem(quit);              NSMenu.PopUpContextMenu(settingsMenu, current, sender as NSView);         }          [Export ("launch:")]         void Launch(NSObject sender)         {             //Use NSAppleScript to add this app to Login item list of macOS         }          [Export ("quit:")]         void Quit(NSObject sender)         {             QuitButtonClicked?.Invoke(this, null);         } 
+        partial void SettingsButtonClick(NSObject sender)         {             var current = NSApplication.SharedApplication.CurrentEvent;              NSMenu settingsMenu = new NSMenu();              NSMenuItem launch = new NSMenuItem("Launch at Login", new ObjCRuntime.Selector("launch:"), "");             NSMenuItem quit = new NSMenuItem("Quit", new ObjCRuntime.Selector("quit:"), "q");              settingsMenu.AddItem(launch);             settingsMenu.AddItem(quit);              NSMenu.PopUpContextMenu(settingsMenu, current, sender as NSView);         }          [Export ("launch:")]         void Launch(NSObject sender)         {             //Use NSAppleScript to add this app to Login item list of macOS.
+            //The app must be in the Applications Folder
+
+            NSAppleScript login = new NSAppleScript("tell application \"System Events\"\n make new login item at end of login items with properties {name: \"Ambar\", path:\"/Applications/Ambar.app\", hidden:false}\n end tell");
+            var errors = new NSDictionary();
+            login.ExecuteAndReturnError(out errors);         }          [Export ("quit:")]         void Quit(NSObject sender)         {             QuitButtonClicked?.Invoke(this, null);         } 
     }
 }
