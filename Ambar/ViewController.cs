7273b9742422c6dbc5a3ab9206d26a9a41a4e5b4
Anagh Sharma <anaghsharma12@gmail.com>
2017-07-11 11:28:40 +0530

    Added Launch to Login logic using NSAppleScript

--- Ambar/ViewController.cs
+++ Ambar/ViewController.cs
@@ -10,6 +10,8 @@ namespace Ambar
         public static event EventHandler AboutMenuItemClicked;
 		NSTrackingArea hoverarea;
 		NSCursor cursor;
+        NSMenuItem launch;
+        bool isLoginItem;
 
         NSAttributedString titleString = new NSAttributedString("Make\nEpic\nThings",
 															   new NSStringAttributes()
@@ -48,16 +50,42 @@ namespace Ambar
             }
         }
 
-        partial void SettingsButtonClick(NSObject sender)         {             var current = NSApplication.SharedApplication.CurrentEvent;              NSMenu settingsMenu = new NSMenu();              NSMenuItem launch = new NSMenuItem("Launch at Login", new ObjCRuntime.Selector("launch:"), "");             NSMenuItem about = new NSMenuItem("About", new ObjCRuntime.Selector("about:"), "");
+        partial void SettingsButtonClick(NSObject sender)         {             var current = NSApplication.SharedApplication.CurrentEvent;              NSMenu settingsMenu = new NSMenu();              launch = new NSMenuItem("Launch at Login", new ObjCRuntime.Selector("launch:"), "");             NSMenuItem about = new NSMenuItem("About", new ObjCRuntime.Selector("about:"), "");
             NSMenuItem quit = new NSMenuItem("Quit", new ObjCRuntime.Selector("quit:"), "q");
               settingsMenu.AddItem(launch);
             settingsMenu.AddItem(about);
-            settingsMenu.AddItem(NSMenuItem.SeparatorItem);             settingsMenu.AddItem(quit);              NSMenu.PopUpContextMenu(settingsMenu, current, sender as NSView);         }          [Export ("launch:")]         void Launch(NSObject sender)         {             //Use NSAppleScript to add this app to Login item list of macOS.
+            settingsMenu.AddItem(NSMenuItem.SeparatorItem);             settingsMenu.AddItem(quit);
+
+			var script = "tell application \"System Events\"\n get the name of every login item\n if login item \"Ambar\" exists then\n return true\n else\n return false\n end if\n end tell";
+			NSAppleScript appleScript = new NSAppleScript(script);
+			var errors = new NSDictionary();
+			NSAppleEventDescriptor result = appleScript.ExecuteAndReturnError(out errors);
+            isLoginItem = result.BooleanValue;
+
+            if (!isLoginItem)
+			{
+				launch.State = NSCellStateValue.Off;
+			}
+            else if (isLoginItem)
+				launch.State = NSCellStateValue.On;
+                         NSMenu.PopUpContextMenu(settingsMenu, current, sender as NSView);         }          [Export ("launch:")]         void Launch(NSObject sender)         {             //Use NSAppleScript to add this app to Login item list of macOS.
             //The app must be in the Applications Folder
 
-            NSAppleScript login = new NSAppleScript("tell application \"System Events\"\n make new login item at end of login items with properties {name: \"Ambar\", path:\"/Applications/Ambar.app\", hidden:false}\n end tell");
-            var errors = new NSDictionary();
-            login.ExecuteAndReturnError(out errors);         }            
+            String script;
+            NSAppleScript login;
+            NSDictionary errors = new NSDictionary();
+            if (!isLoginItem)
+            {
+                script = "tell application \"System Events\"\n make new login item at end of login items with properties {name: \"Ambar\", path:\"/Applications/Ambar.app\", hidden:false}\n end tell";
+                login = new NSAppleScript(script);
+                login.ExecuteAndReturnError(out errors);
+            }
+            else
+            {
+				script = "tell application \"System Events\"\n delete login item \"Ambar\"\n end tell";
+				login = new NSAppleScript(script);
+				login.ExecuteAndReturnError(out errors);
+            }         }            
         [Export ("about:")]
         void About(NSObject sender)
         {
